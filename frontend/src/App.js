704147694555549bd4b6863c515ace3cb387161a
OSova2023 <ovf2019@gmail.com>
2023-11-14 18:06:42 +0300

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,12 @@ import Search from "./components/Search";
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const [images, setImages] = useState([]);
+
+  console.log(images);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
@@ -13,11 +19,15 @@ function App() {
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        setImages([data, ...images]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     setWord("");
   };
-  const [word, setWord] = useState("");
+
   return (
     <div>
       <Header title="Images GALARY" />
